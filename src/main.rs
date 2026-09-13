0c6694f18a29f779c2e4f75cd965bd067dd19dab
zevinto <zevinto@163.com>
2026-09-13 21:47:18 +0800

    feat: 迭代器改进

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::{env, process};
 use minigrep;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
